da743987846928e04257ecbff1cd1598dc8b2ebc
stets1988 <stetsiuk.pavel@gmail.com>
2019-09-24 00:30:14 +0300

    feat:add new function decode

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+
 const MORSE_TABLE = {
     '.-':     'a',
     '-...':   'b',
@@ -37,10 +38,52 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+var k;
+var expr="";
+var substr3 = "";
+var substr4 = "";
 function decode(expr) {
-    // write your solution here
+var newarr =  Object.entries(MORSE_TABLE);  
+	for(var i=0; i < expr.length;)
+	{	
+		var substr = expr.slice((i),(i+10));
+			for(var j=0; j < substr.length;)
+			{
+				var substr2 = substr.slice(j,(j+2));
+					if(substr2 == "00")
+					{
+					substr3="";
+					}
+					if(substr2 == "10")
+					{
+					substr3=substr3+".";
+					}
+					if(substr2 == "11")
+					{
+					substr3=substr3+"-";	
+					}
+					if(substr2 == "**")
+					{
+					substr3="";
+					}
+			j+=2;
+			}
+		for(k=0; k<newarr.length; k++)
+		{	
+			if(substr3 == (newarr[k][0]+""))
+			{
+				substr4=substr4+newarr[k][1];
+			}
+		}
+			if(substr3 == "")
+			{
+				substr4=substr4+" ";
+			}
+	i+=10;
+	}
+return substr4;
 }
-
 module.exports = {
     decode
 }
+//decode();
